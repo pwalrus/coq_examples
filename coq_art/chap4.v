0e00b426e8765aadd388e162ca81d0e2b83147bc
p walrus <possible_walrus@protonmail.com>
2023-01-07 15:35:13 -0700

    coqart chp4 ex5

--- coq_art/chap4.v
+++ coq_art/chap4.v
@@ -21,4 +21,67 @@ Section ex42.
 End ex42.
 
 
+Section ex43.
+  Parameters (A:Set)(P Q: A -> Prop) (R: A -> A -> Prop).
+
+  Theorem all_perm : (forall a b:A, R a b) -> (forall a b : A, R b a).
+  Proof.
+    intros H0.
+    intros a b.
+    apply H0.
+  Qed.
+
+  Theorem all_imp_dist : (forall a : A, P a -> Q a) -> (forall a: A, P a) -> (forall a : A, Q a).
+  Proof.
+    intros H0 H1 a.
+    assert (H2 : P a).
+    apply H1.
+    apply H0.
+    exact H2.
+  Qed.
+
+  Theorem all_delta : (forall a b : A, R a b) -> (forall a : A, R a a).
+  Proof.
+    intros H0 a.
+    apply H0 with (a:= a) (b := a).
+  Qed.
+End ex43.
+
+Require Import ZArith.
+Require Import Znat.
+
+Section ex44.
+
+  Definition id: forall A : Set, A -> A := fun _ x => x.
+
+  Definition diag : forall A B : Set, (A -> A -> B) -> (A -> B)
+    := fun _ _ f x => f x x.
+
+  Definition permute : forall A B C : Set, (A -> B -> C) -> B -> A -> C
+    := fun _ _ _ f x y => f y x.
+
+  Definition f_nat_x : forall A : Set, (nat -> A) -> Z -> A
+    := fun _ f x => f (Z.to_nat x).
+
+End ex44.
+
+Section ex45.
+  Theorem all_perm45 : forall(A: Type)(P: A->A->Prop), (forall a b:A, P a b) -> (forall a b : A, P b a).
+  Proof.
+    intros A P H0.
+    intros a b.
+    apply H0.
+  Qed.
+
+  Theorem resolution :forall(A: Type)(P Q R S : A -> Prop),
+    (forall a: A, Q a -> R a -> S a) -> (forall b : A, P b -> Q b) -> (forall c : A, P c -> R c -> S c).
+  Proof.
+    intros A P Q R S H0 H1 c.
+    intros H2 H3.
+    apply H0.
+    apply H1.
+    exact H2.
+    exact H3.
+  Qed.
+End ex45.
 
