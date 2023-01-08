cbde8321fec4f75e64498ab68fbee79b70d4a039
p walrus <possible_walrus@protonmail.com>
2023-01-08 14:38:10 -0700

    coqart chp5 ex7

--- coq_art/chap5.v
+++ coq_art/chap5.v
@@ -144,4 +144,127 @@ Section ex56.
 
 End ex56.
 
+Section ex57.
+
+  Definition pierce := forall P Q : Prop, ((P -> Q) -> P) -> P.
+  Definition classic := forall P : Prop, ~~P -> P.
+  Definition excluded_middle := forall P : Prop, ~P \/ P.
+  Definition demorgan_not_and_not := forall P Q : Prop, ~(~P/\~Q) -> P\/Q.
+  Definition implies_to_or := forall P Q : Prop, (P->Q)->(~P\/Q).
+
+  Theorem classic0 : excluded_middle -> classic.
+  Proof.
+    intros H0 P H1.
+    case H0 with (P:=P).
+    intros H2; elim H1; exact H2.
+    intros H2; exact H2.
+  Qed.
+
+  Theorem classic1 : classic -> excluded_middle.
+  Proof.
+    intros H0 P.
+    cut (P \/ ~P).
+    intros H1.
+    case H1; [intros H2; right; exact H2 | intros H2; left; exact H2].
+    apply H0 with (P:= (P\/~P)).
+    apply d_neg_lem with (A:= P).
+  Qed.
+
+  Theorem classic2 : implies_to_or -> excluded_middle.
+  Proof.
+    intros H0 P.
+    apply H0.
+    intros H1; exact H1.
+  Qed.
+
+  Theorem classic3 : excluded_middle -> implies_to_or.
+  Proof.
+    intros H0 P Q H1.
+    case H0 with (P:=P).
+    intros H2; left; exact H2.
+    intros H2; right; apply H1; exact H2.
+  Qed.
+
+  Theorem classic4 : excluded_middle -> demorgan_not_and_not.
+  Proof.
+    intros H0 P Q H1.
+    case H0 with (P:=P).
+    intro H3.
+    case H0 with (P:=Q).
+    intro H4. elim H1.
+    split; [exact H3 | exact H4].
+    intros H4; right; exact H4.
+    intros H4; left; exact H4.
+  Qed.
+
+  Theorem classic5 : demorgan_not_and_not -> excluded_middle.
+  Proof.
+    intros H0 P.
+    apply H0 with (P:= ~P) (Q:=P).
+    intros H1.
+    destruct H1 as [H2 H3]; elim H2; exact H3.
+  Qed.
+
+  Lemma classic3_rev : forall P Q : Prop, (~P\/Q)->(P->Q).
+  Proof.
+    intros P Q H0 H1.
+    case H0.
+    intro H2; elim H2; exact H1.
+    intro H2; exact H2.
+  Qed.
+
+  Lemma non_premise : forall P Q : Prop, ~P -> (P -> Q).
+  Proof.
+    intros P Q H4 H5; elim H4; exact H5.
+  Qed.
+
+  Theorem classic6 : excluded_middle -> pierce.
+  Proof.
+    intros H0 P Q H1.
+    case H0 with (P:=P).
+    intro H2.
+    assert (H3 : P -> Q).
+    apply non_premise; exact H2.
+    apply H1; exact H3;
+    assert (H3: ~P -> (P -> Q)).
+    intro H2; exact H2.
+  Qed.
+
+  Lemma demorgan1 : excluded_middle -> forall P Q : Prop, ~(P \/ Q) -> ~P /\ ~Q.
+  Proof.
+    intros H0 P Q H1.
+    split.
+    case H0 with (P:=P).
+    intros H2; exact H2.
+    intros H3; elim H1; left; exact H3.
+    case H0 with (P:=Q).
+    intros H2; exact H2.
+    intros H2. elim H1; right; exact H2.
+  Qed.
+
+  Lemma make_false : forall P: Prop, ~P -> (P -> False).
+  Proof.
+    intros P H0 H1.
+    elim H0; exact H1.
+  Qed.
+
+  Lemma rem_false : forall P: Prop, (P -> False) -> ~P.
+  Proof.
+    intros P H0.
+    intro H1; apply H0; exact H1.
+  Qed.
+
+  Theorem classic7 : pierce -> excluded_middle.
+  Proof.
+    intros H0 P.
+    assert (H2: ((~P \/ P) -> False) -> ~P).
+    intros H3; apply rem_false; intros H4; apply H3; right; exact H4.
+    assert (H4: ((~P \/ P) -> False) -> ~P \/ P).
+    intros H5; left; apply H2; exact H5.
+    apply H0 with (P := ~P \/ P) (Q:= False).
+    exact H4.
+  Qed.
+
+End ex57.
+
 
