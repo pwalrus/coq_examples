22a54e292c792a274b0b462df45c9dd440ee97f6
p walrus <possible_walrus@protonmail.com>
2023-01-07 21:16:07 -0700

    coqart chp5 ex6

--- coq_art/chap5.v
+++ coq_art/chap5.v
@@ -71,3 +71,77 @@ Section ex54.
   Qed.
 End ex54.
 
+Section ex55.
+
+  Theorem many_or : forall (A:Set)(a b c d : A), a=c \/ b=c \/ c=c \/ d=c.
+  Proof.
+    intros A a b c d.
+    right.
+    right.
+    left.
+    reflexivity.
+  Qed.
+
+End ex55.
+
+Section ex56.
+  Theorem conj_assoc : forall A B C : Prop, A /\ (B/\C) -> (A/\B)/\C.
+  Proof.
+    intros A B C H0.
+    destruct H0 as [A0 H1].
+    destruct H1 as [B0 C0].
+    split.
+    split.
+    assumption.
+    assumption.
+    assumption.
+  Qed.
+
+  Theorem double_imp : forall A B C D: Prop, (A->B)/\(C->D) /\ A /\ C -> B/\D.
+  Proof.
+    intros A B C D H0.
+    destruct H0 as [H1 H2].
+    destruct H2 as [H3 H4].
+    destruct H4 as [H5 H6].
+    split; [apply H1; exact H5 | apply H3; exact H6].
+  Qed.
+
+  Theorem not_contra1 : forall A : Prop, ~(A /\ ~A).
+  Proof.
+    intros A H0.
+    destruct H0 as [H1 H2].
+    elim H2; exact H1.
+  Qed.
+
+  Theorem disj_assoc : forall A B C : Prop, A \/ (B \/ C) -> (A \/ B) \/ C.
+  Proof.
+    intros A B C H0.
+    case H0.
+    intros H1; left; left; exact H1.
+    intros H2; case H2.
+    intros H1; left; right; exact H1.
+    intros H1; right; exact H1.
+  Qed.
+
+  Theorem d_neg_lem : forall A : Prop, ~~(A\/~A).
+  Proof.
+    intros A H0.
+    elim H0.
+    right.
+    intros H1.
+    elim H0.
+    left; exact H1.
+  Qed.
+
+  Theorem or_and_not : forall A B : Prop, (A \/ B) /\ ~A -> B.
+  Proof.
+    intros A B H0.
+    destruct H0 as [H1 H2].
+    case H1.
+    intros H3; elim H2; exact H3.
+    intros H3; exact H3.
+  Qed.
+
+End ex56.
+
+
